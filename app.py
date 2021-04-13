de949a437b5113f73eea89609a106f2ec47e4661
Tor Kingdon <tor@hearkitty.com>
2021-04-12 18:43:18 -0600

    almost working

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 from random import randint, choice, sample
 from flask_debugtoolbar import DebugToolbarExtension
 from stories import Story, storylist, make_story_from_class
+# import stories
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "thecheddarthebetter1969"
@@ -12,6 +13,7 @@ debug = DebugToolbarExtension(app)
 def pick_story():
 	titles = []
 	for story in storylist:
+	# for story in stories.storylist:
 		for key in story:
 			titles.append(key)
 
@@ -20,20 +22,20 @@ def pick_story():
 @app.route('/get-words')
 def get_words():
 	"""prepare prompts and return homepage"""
-	chosen_story = make_story_from_class(request.args["story"])
+	chosen_title = request.args["story"]
+	# chosen_story = stories.make_story_from_class(chosen_title)
+	chosen_story = make_story_from_class(chosen_title)
 	prompts = []
 	for prompt in chosen_story.prompts:
 		prompts.append(prompt.replace('_', ' '))
 
-	return render_template("get-words.html", prompts = prompts, key_prompts = chosen_story.prompts, num_of_prompts = len(prompts))
+	return render_template("get-words.html", story = chosen_story, prompts = prompts, key_prompts = chosen_story.prompts, num_of_prompts = len(prompts))
 
 @app.route('/story')
 def write_story():
 	"""takes input from querystring and 'writes' the story by inserting the user-entered words into the story"""
-	chosen_story = make_story_from_class("Once Upon a Time")
-	# answers = {}
-	# for query in request.args:
-	# 	answers[query] = request.args[query]
+	# from stories import chosen_story
+	# getattr(wtf_object, request.args['key'])
 	finished_story = chosen_story.generate(answers)
 
 	return render_template("story.html", finished_story = finished_story)
